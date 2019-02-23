19d75b305cd1379aa607a81f7f9f4fc4649ac446
sujiayi317 <sujiayi317@gmail.com>
2019-02-22 20:35:07 -0500

    create html templates for home&about page

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,34 @@
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)     # app is an instance of class Flask
+
+posts = [
+    {
+        'author': 'Jiayi Su',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'February 20, 2019'
+    },
+    {
+        'author': 'Bernard Dong',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'February 21, 2019'
+    }
+]
 
 
 @app.route("/")           # decorator route is what we type into our browser to go to different pages
-def hello():
-    return "<h1>Home Page!</h1>"  # html heading 1
+@app.route("/home")
+def home():
+    # return "<h1>Home Page</h1>"  # html heading 1
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")           # decorator route is what we type into our browser to go to different pages
+def about():
+    # return "<h1>About Page</h1>"  # html heading 1
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
